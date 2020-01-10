3d5f682fd3b8f27e8e2c96fe38125ba9e34ea3df
jayesh293927 <jayesh293927@gmail.com>
2020-01-10 18:39:43 +0530

    add photo in about page and changes design

--- admin/manage_video.php
+++ admin/manage_video.php
@@ -1,9 +1,70 @@
 <?php
+session_start();
 include_once('ui_utility.php');
+if (!isset($_SESSION['login_user'])){
+    header("Location:index.php");
+}
 ?>
+<head>
+    <title>Manage Videos</title>
+       <!-- Bootstrap Core CSS -->
+       <link href="../css/bootstrap.min.css" rel="stylesheet">
+
+      <!-- Custom CSS -->
+      <link href="../css/modern-business.css" rel="stylesheet">
+</head>
+
 <script src="../js/jquery.toaster.js"></script>
+<script type="text/javascript" src="../js/jquery.form.js"></script>
  <script>
+  function createCol(video_path, video_id) {
+            return `<div class="col-md-6 text-center">
+                <div class="thumbnail">
+                    <iframe class="embed-responsive-item" allowfullscreen src="` + video_path + `">
+                    </iframe>
+                    <button id="` + video_id +`" class="btn btn-sm btn-danger" type="button" data-toggle="modal" 
+                    data-target="#confirmDelete" data-title="Delete User" data-message="Are you sure you want to delete `+video_path+` ?">
+                    <i class="glyphicon glyphicon-trash"></i> Delete
+                    </button>
+                </div>
+            </div>`;
+    }
+
+    function get_video() {
+        $.ajax({
+                url: "video_details.php",
+                type: "GET",
+                data: {'action' : 'get_video_details'},
+                success: function (response) {
+                    $('#videos').html("");
+                    video_details_obj = JSON.parse(response);
+                    for (vid_obj in video_details_obj) {
+                        iCol = "";
+                        iCol = createCol(video_details_obj[vid_obj]['video_path'], video_details_obj[vid_obj]['video_id']);
+                        $('#videos').append($(iCol));
+                    }
+                }
+            });
+    }
         $(document).ready(function() { 
+
+            $('#uploadVideo').click(function(){
+            $('#viewVideoContainer').hide();
+            $('#uploadVideoContainer').show();
+        });
+        
+        $('#viewVideo').click(function(){
+            $('#uploadVideoContainer').hide();
+            $('#viewVideoContainerContainer').show();
+            get_video();
+        });
+
+        $('#deleteVideo').click(function(){
+            $('#uploadVideoContainer').hide();
+            $('#viewVideoContainerContainer').hide();
+            $('#deleteVideoContainer').show();
+        });
+        
             $("#frmAddVideo").submit(function(event){
                 if ($("[class='error']").length > 0) {
                     $("[class='error']").remove();
@@ -22,6 +83,7 @@ include_once('ui_utility.php');
                     success: function (response) {//response is value returned from php 
                         $.toaster({ message : 'video added successfully', title : 'Success', priority : 'success' });
                         $("#video_path").val("");
+
                     },
                     error: function() {
                         $.toaster({ message : 'Failed to add video', title : 'Error', priority : 'danger' });
@@ -29,23 +91,88 @@ include_once('ui_utility.php');
                 });
                 event.preventDefault();
             });
+        $('#confirmDelete').on('show.bs.modal', function (e) {
+                $message = $(e.relatedTarget).attr('data-message');
+                $(this).find('.modal-body p').text($message);
+                $title = $(e.relatedTarget).attr('data-title');
+                $(this).find('.modal-title').text($title);
+            
+                // Pass form reference to modal for submission on yes/ok
+                $(this).find('.modal-footer #confirm').data('video_id', $(e.relatedTarget)[0]['id']);
+        });
+            
+        $('#confirmDelete').find('.modal-footer #confirm').on('click', function(){
+            video_id = $(this).data('video_id');
+            $.ajax({
+                type: 'post',
+                url: 'video_details.php',
+                data: {'action': 'delete_video', 'video_id': video_id},
+                success: function (response) {
+                  jObject = JSON.parse(response);
+                  if(jObject['Success']) {
+                    $("#confirmDelete").modal("hide");
+                    $.toaster({ message : jObject['Success'], title : 'Success', priority : 'success' });
+                    get_video();
+                  } else {
+                      $.toaster({ message :  jObject['error']['msg'], title : 'Error', priority : 'danger' });
+                  } 
+                },
+                error: function() {
+                    $.toaster({ message : 'Failed to delete Video', title : 'Error', priority : 'danger' });
+                }
+            });
+        });
         });
 </script>
-
-<div class="container">
-    <form id="frmAddVideo">
-        <div class="row">
-            <div class="form-group required">
-                    <label class="control-label themelabel" for="video_path">Video Path</label>
-                    <input name="video_path" id="video_path" type="text"  class="form-control">
+<body>
+    <div class="container">
+        <br>
+            <div class="row">
+                <button id="uploadVideo" type="button" class="btn btn-primary">Upload Video</button>
+                <button id="viewVideo" type="button" class="btn btn-primary">View Videos</button>
             </div>
-        </div>
+            <br>
 
-        <div class="row">
-            <div class="form-group">
-                        <input type="submit" name="submit" value="Add Video"
-                        class="btn btn-primary"></span>
+        <div id="uploadVideoContainer" style="display:none;">
+            <form id="frmAddVideo">
+            <div class="row">
+                <div class="form-group required">
+                        <label class="control-label themelabel" for="video_path">Video Path</label>
+                        <input name="video_path" id="video_path" type="text"  class="form-control">
+                </div>
             </div>
+
+            <div class="row">
+                <div class="form-group">
+                            <input type="submit" name="submit" value="Add Video"
+                            class="btn btn-primary"></span>
+                </div>
+            </div>
+        </form>
         </div>
-    </form>
-</div>
+        
+    </div>
+    <!-- <div id="viewVideoContainer" style="display:none;"> -->
+    <div id="videos">
+        </div>    
+    <!-- </div> -->
+
+    
+    <div class="modal fade" id="confirmDelete" role="dialog" aria-labelledby="confirmDeleteLabel" aria-hidden="true">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                <h4 class="modal-title">Delete Parmanently</h4>
+              </div>
+              <div class="modal-body">
+                <p>Are you sure about this ?</p>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+                <button type="button" class="btn btn-danger" id="confirm">Delete</button>
+              </div>
+            </div>
+          </div>
+        </div>
+</body>
--- admin/video_details.php
+++ admin/video_details.php
@@ -1,38 +1,61 @@
 <?php
 session_start(); // Starting Session
 include_once('utility.php');
+//var_dump($_POST);
 
-if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    if ($_GET['action'] == 'get_video_details') {
-        $video_details = array();
-        $video_details_obj = array();
-        $connection = getConnection();
-        $query = mysqli_query($connection, "select video_id, video_path from videodetails");
-        $counter=1;
-        while ($row = $query->fetch_assoc()) {
-            if (mysqli_num_rows($query)>0){
-                $video_details_obj["video_id"] =  $row["video_id"];
-                $video_details_obj["video_path"] = $row["video_path"];
-                array_push($video_details, $video_details_obj);
+try {
+    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+        if ($_GET['action'] == 'get_video_details') {
+            $video_details = array();
+            $video_details_obj = array();
+            $connection = getConnection();
+            $query = mysqli_query($connection, "select video_id, video_path from videodetails");
+            $counter=1;
+            while ($row = $query->fetch_assoc()) {
+                if (mysqli_num_rows($query)>0){
+                    $video_details_obj["video_id"] =  $row["video_id"];
+                    $video_details_obj["video_path"] = $row["video_path"];
+                    array_push($video_details, $video_details_obj);
+                }
+            }
+            echo json_encode($video_details);
+        }
+    }
+    else if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        if ($_POST['action'] == 'add_video') {
+            $message="";
+            $video_path = $_POST['video_path'];
+            $connection = getConnection();
+            $insert_query = "insert into videodetails (video_path) values ('". $video_path."')";
+                if(mysqli_query($connection, $insert_query)) {
+                    $message = "record inserted";
+                }else{
+                    $message = "record not inserted";
+                }
+        }
+        else if ($_POST['action'] == 'delete_video') {
+            $video_id = $_POST["video_id"];
+            $delete_query = "delete from videodetails where video_id=". $video_id;
+            $connection = getConnection();
+            if(mysqli_query($connection, $delete_query)) {
+                $message = "Video deleted successfully";
+                getSuccessMsg($message);
+            }else{
+                $message = "Failed to delete Video";
+                throw new Exception($message);
             }
         }
-        mysqli_close($connection);
-        echo json_encode($video_details);
     }
-}
-else if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if ($_POST['action'] == 'add_video') {
-        $message="";
-        $video_path = $_POST['video_path'];
-        $connection = getConnection();
-        $insert_query = "insert into videodetails (video_path) values ('". $video_path."')";
-            if(mysqli_query($connection, $insert_query)) {
-                $message = "record inserted";
-            }else{
-                $message = "record not inserted";
-            }
+   
+} 
+catch (exception $e) {
+    getErrorMsg($e);
+} 
+finally {
+    // Closing Connection
+    if(isset($connection)){
+        mysqli_close($connection); 
     }
-  mysqli_close($connection); // Closing Connection
 }
 
 ?>
